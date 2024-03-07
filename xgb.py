441d5b737a2bc26bc6b898efb924fbfa910cea02
Barbara Santos <pro@barbarasan.com>
2024-03-07 07:56:54 +0000

    Update xgb.py

--- xgb.py
+++ xgb.py
@@ -5,27 +5,28 @@ import pickle
 import warnings
 warnings.simplefilter(action='ignore')
 
-df_train = pd.read_csv("twitch_old.csv")
+def generate_model():
+    df_train = pd.read_csv("twitch_old.csv")
 
-X_train, y_train = df_train.drop(columns=['rating']), df_train['rating']
+    X_train, y_train = df_train.drop(columns=['rating']), df_train['rating']
 
-y_train /=100
+    y_train /=100
 
-with open("pca.pkl","rb") as file:
-    pca = pickle.load(file)
-    file.close()
+    with open("pca.pkl","rb") as file:
+        pca = pickle.load(file)
+        file.close()
 
-X_train = pca.transform(X_train)
+    X_train = pca.transform(X_train)
 
-best = {'objective': 'reg:logistic',
-        'n_estimators': 30,
-        'min_child_weight': 20,
-        'gamma': 0.2,
-        'seed': 123}
+    best = {'objective': 'reg:logistic',
+            'n_estimators': 30,
+            'min_child_weight': 20,
+            'gamma': 0.2,
+            'seed': 123}
 
-model = XGBRegressor(**best)
-model.fit(X_train, y_train)
+    model = XGBRegressor(**best)
+    model.fit(X_train, y_train)
 
-with open("model.pkl","wb") as file:
-    pickle.dump(model, file)
-    file.close()
+    with open("model.pkl","wb") as file:
+        pickle.dump(model, file)
+        file.close()
